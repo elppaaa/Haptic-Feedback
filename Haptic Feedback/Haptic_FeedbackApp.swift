16f4b659c3512ff71a70724adc27b58d0ac8e789
elppaaa <withbcd@gmail.com>
2022-02-20 21:22:51 +0900

    Initial Commit

---
+++ Haptic Feedback/Haptic_FeedbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Haptic_FeedbackApp.swift
+//  Haptic Feedback
+//
+//  Created by JK on 2022/02/20.
+//
+
+import SwiftUI
+
+@main
+struct Haptic_FeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
